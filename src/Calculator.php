ebc90c12759960b7332bda0cacfb5498f449cd13
Ваня Цимбалист <62908238+Tsim0710@users.noreply.github.com>
2025-11-19 16:12:37 +0300

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -10,4 +10,14 @@ final class Calculator
     {
         return $a + $b;
     }
+
+    public function multiply(int $a, int $b): int
+    {
+        // Небольшая логика, чтобы были ветки
+        if ($a === 0 || $b === 0) {
+            return 0;
+        }
+
+        return $a * $b;
+    }
 }
