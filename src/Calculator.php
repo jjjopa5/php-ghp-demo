9876972dde3e8d8e4fc199f10df66388e23a0e53
Ваня Цимбалист <62908238+Tsim0710@users.noreply.github.com>
2025-12-05 00:59:34 +0300

    Update Calculator.php

--- src/Calculator.php
+++ src/Calculator.php
@@ -6,8 +6,18 @@ namespace App;
 
 final class Calculator
 {
-    public function add(int $a , int $b): int
+    public function add(int $a, int $b): int
     {
         return $a+$b;
+    }
+
+    public function multiply(int $a, int $b): int
+    {
+        // Небольшая логика, чтобы были ветки
+        if ($a === 0 || $b === 0) {
+            return 0;
+        }
+
+        return $a * $b;
     }
 }
